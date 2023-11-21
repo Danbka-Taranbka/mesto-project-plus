ad8367a64e80844808a2fa31b311bcefaeddb115
Danbka-Taranbka <danila.klim01@gmail.com>
2023-11-21 23:08:49 +0300

    cards and users controllers add + error-middlewar

--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -1,26 +1,53 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import Card from "../models/card";
 
 /*const createCardd = (req: Request, res: Response) => {
   console.log(req.user._id);
 };*/
 
-export const getCards = (_req: Request, res: Response) => {
+export const getCards = (_req: Request, res: Response, next: NextFunction) => {
   return Card.find({})
-    .then((cards) => res.status(200).send({data: cards}))
-    .catch(() => res.status(500).send({message: "Server Error!"}));
+    .then((cards) => res.status(200).send(cards))
+    .catch(next);
 };
 
-export const createCard = (req: Request, res: Response) => {
-  const {name, link, owner} = req.body;
+export const createCard = async (req: Request, res: Response, next: NextFunction) => {
+  const {name, link} = req.body;
 
-  return Card.create({name, link, owner})
-    .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({message: "Server Error!"}));
+  return (await Card.create({name, link, owner: req.body.user._id}))
+    .populate("owner")
+    .then((card) => res.status(201).send(card))
+    .catch(next);
 };
 
-export const deleteCard = (req: Request, res: Response) => {
+export const deleteCard = (req: Request, res: Response, next: NextFunction) => {
   return Card.findByIdAndRemove(req.params.id)
-    .then(card => res.send({data: card}))
-    .catch(() => res.status(500).send({message: "Server Error!"}));
+    .orFail(() => {
+      throw res.status(404).send({message: 'There is no card with such _id!'});
+    })
+    .then(card => res.status(200).send(card))
+    .catch(next);
+};
+
+export const putLike = (req: Request, res: Response, next: NextFunction) => {
+  return Card.findByIdAndUpdate(
+    req.params.id,
+    { $addToSet: { likes: req.body.user._id } },
+    { new: true }
+  )
+    .orFail(() => {
+      throw res.status(404).send({message: 'There is no card with such _id!'});
+    })
+    .then(card => res.status(200).send(card))
+    .catch(next);
+};
+
+export const removeLike = (req: Request, res: Response, next: NextFunction) => {
+  return Card.findByIdAndUpdate(
+    req.params.id,
+    { $pull: { likes: req.body.user._id } },
+    { new: true }
+  )
+    .then(card => res.status(200).send(card))
+    .catch(next);
 };
